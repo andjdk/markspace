f7feef9d2ee35806556b0be2b42cdfea5db44af6
andjdk <284521147@qq.com>
2018-05-25 11:48:43 +0800

    初始版本

--- nas/markspace/MarkSpace/contract/contract.markspace.js
+++ nas/markspace/MarkSpace/contract/contract.markspace.js
@@ -1,101 +1,140 @@
 "use strict";
 
 var MarkSpaceItem = function(text) {
-	if (text) {
+	if(text) {
 		var obj = JSON.parse(text);
 		this.postImageSource = obj.postImageSource;
 		this.postContent = obj.postContent;
 		this.postDate = obj.postDate;
-        this.postID = obj.postID;
+		this.postID = obj.postID;
 		this.author = obj.author;
+		this.userName = obj.userName;
 	} else {
-	    this.postImageSource = "";
-	    this.postContent = "";
-	    this.postDate = "";
-        this.postID = "";
-        this.author = "";
+		this.postImageSource = "";
+		this.postContent = "";
+		this.postDate = "";
+		this.postID = "";
+		this.author = "";
+		this.userName = "";
 	}
 };
 
 MarkSpaceItem.prototype = {
-	toString: function () {
+	toString: function() {
 		return JSON.stringify(this);
 	}
 };
 
-var MarkSpace = function () {
+var MarkSpace = function() {
 
-    LocalContractStorage.defineProperty(this, "size");
+	LocalContractStorage.defineProperty(this, "size");
 
-    LocalContractStorage.defineMapProperty(this, "arrayMap", {
-        parse: function (text) {
-            return new MarkSpaceItem(text);
-        },
-        stringify: function (o) {
-            return o.toString();
-        }
-    });
+	LocalContractStorage.defineMapProperty(this, "arrayMap", {
+		parse: function(text) {
+			return new MarkSpaceItem(text);
+		},
+		stringify: function(o) {
+			return o.toString();
+		}
+	});
+
 };
 
 MarkSpace.prototype = {
-    init: function () {
-        this.size = 1;
-    },
+	init: function() {
+		this.size = 1;
+	},
 
-    len:function(){
-      return this.size;
-    },
+	len: function() {
+		return this.size;
+	},
 
-    save: function (postImageSource, postContent,postDate) {
+	save: function(postImageSource, postContent, postDate, postUserName) {
 
-        postImageSource = postImageSource;
-        postContent = postContent;
-        postDate = postDate.trim();
-        if (postImageSource === "" || postContent === "" || postDate === ""){
-            throw new Error("empty postImageSource / postContent / postDate");
-        }
+		postImageSource = postImageSource;
+		postContent = postContent;
+		postDate = postDate.trim();
+		if(postImageSource === "" || postContent === "" || postDate === "") {
+			throw new Error("empty postImageSource / postContent / postDate");
+		}
 
-        var from = Blockchain.transaction.from;
+		var from = Blockchain.transaction.from;
 
-        var index = this.size;
+		var index = this.size;
 
-        var markspaceItem = new MarkSpaceItem();
-        markspaceItem.author = from;
-        markspaceItem.postImageSource = postImageSource;
-        markspaceItem.postContent = postContent;
-        markspaceItem.postDate = postDate;
-        markspaceItem.postID = index;
-        
-        this.arrayMap.put(index, markspaceItem);
-        this.size +=1;
+		var markspaceItem = new MarkSpaceItem();
+		markspaceItem.author = from;
+		markspaceItem.postImageSource = postImageSource;
+		markspaceItem.postContent = postContent;
+		markspaceItem.postDate = postDate;
+		markspaceItem.postID = index;
+		markspaceItem.userName = postUserName;
 
-    },
+		this.arrayMap.put(index, markspaceItem);
+		this.size += 1;
 
-    get: function (postID) {
+	},
 
-        if ( postID <= 0 ) {
-            throw new Error("postID [1..]")
-        }
-        return this.arrayMap.get(postID);
-    },
+	getDetail: function(postID) {
 
-    getList: function(offset, limit) {
-        offset = parseInt(offset);
-        limit = parseInt(limit);
-        if(offset>this.size){
-           throw new Error("offset is not valid");
-        }
-        var number = offset+limit;
-        if(number > this.size){
-          number = this.size;
-        }
-        var result = new Array();
-        for(var i=offset;i<number;i++){
-            var text = this.arrayMap.get(i);
-            result.push(text);
-        }
-        return result;
-    }
-    
+		if(postID <= 0) {
+			throw new Error("postID [1..]")
+		}
+		return this.arrayMap.get(postID);
+	},
+
+	getPrivate: function(author,offset, limit) {
+		offset = parseInt(offset);
+		limit = parseInt(limit);
+		if(offset > this.size) {
+			throw new Error("offset is not valid");
+		}
+		if(author = "") {
+			throw new Error("wallet address is empty")
+		}
+		
+		var number = offset + limit;
+		if(number > this.size) {
+			number = this.size;
+		}
+		
+		var result = new Array();
+		if(this.size > 0) {
+			for(var i = 0; i < this.size; i++) {
+				var text = this.arrayMap.get(i);
+				if(!text && typeof(text) != "undefined" && text != 0) {
+					break;
+				}
+				if(author == text.author) {
+					result.push(text)
+				}
+			}
+		}
+		result.reverse();
+		return result
+	},
+
+	getList: function(offset, limit) {
+		offset = parseInt(offset);
+		limit = parseInt(limit);
+		if(offset > this.size) {
+			throw new Error("offset is not valid");
+		}
+		var number = offset + limit;
+		if(number > this.size) {
+			number = this.size;
+		}
+		var result = new Array();
+		for(var i = offset; i < number; i++) {
+			var text = this.arrayMap.get(i);
+			if(!text && typeof(text) != "undefined" && text != 0) {
+				break;
+			}
+			result.push(text);
+		}
+		result.reverse();
+		return result;
+	}
+
 };
 module.exports = MarkSpace;
